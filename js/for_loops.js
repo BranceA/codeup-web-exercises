4c5afae4a590c1e7bc2a23d995e729228538800e
BranceA <brancealexander@gmail.com>
2019-08-13 13:31:48 -0500

    much of loops complete

---
+++ js/for_loops.js
@@ -0,0 +1,32 @@
+var i;
+
+function showMultiplicationTable(num) {
+    for (i = 1; i <= 10; i++) {
+        console.log(num * i);
+    }
+}
+
+// showMultiplicationTable(7);
+
+function isRandomNumberOdd(random) {
+    for (k = 1; k <= 10; k++) {
+        var random = Math.floor(Math.random() * 181) + 20;
+        if (random % 2 === 1) {
+            console.log(random + " is an odd number!");
+        } else {
+            console.log(random + " is an even number!");
+        }
+    }
+}
+
+// isRandomNumberOdd();
+
+var num5 = 1;
+
+function concatNumbers(num5) {
+    for (num5 = 1; num5 < 10; num5++) {
+        console.log(num5.toString().repeat(num5));
+    }
+}
+
+concatNumbers();
--- js/while.js
+++ js/while.js
@@ -4,3 +4,19 @@ while (i <= 65536) {
     console.log(i);
     i *=2;
 }
+
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+var conesSold = Math.floor(Math.random() * 5) + 1;
+
+do {
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+    if (conesSold > allCones) {
+        console.log("Sorry. I can't sell you " + conesSold + " I only have " + allCones + " left.");
+    } else {
+        allCones -= conesSold;
+        console.log("Here you are! " + conesSold + " cone(s)! Only " + allCones + " left to sell!")
+    }
+} while(allCones > 0);
