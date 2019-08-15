9e966c0ce6dc4c13daffe628eeb266ed0d6ec7ea
BranceA <brancealexander@gmail.com>
2019-08-15 10:08:28 -0500

    circles will now be assigned names

--- js/circle.js
+++ js/circle.js
@@ -1,6 +1,8 @@
 (function() {
     "use strict";
 
+    var names = ["Tim", "Bob", "Sarah", "Bertha", "Jim", "Bart", "Sally", "Juan", "Doug", "Patty", "David", "Steven", "Erika", "Gertrude", "Nemo", "Harry", "Larry", "Mo", "Heather", "Brance Jr."];
+
     // create a circle object
     var circle = {
         radius: 3,
@@ -29,7 +31,7 @@
     };
 
     // log info about the circle
-    console.log("This is a circle. It has a radius of " + circle.radius + ".");
+    console.log("This is a circle. It's name is " + names[Math.floor(Math.random() * 21)] + ". It has a radius of " + circle.radius + ".");
     circle.logInfo(false);
     console.log("Circle area rounded to the nearest whole number");
     circle.logInfo(true);
@@ -38,7 +40,7 @@
     // TODO: Change the radius of the circle to 5.
     circle.radius = 5;
     // log info about the circle
-    console.log("This is a circle. It has a radius of " + circle.radius + ".");
+    console.log("This is a circle. It's name is " + names[Math.floor(Math.random() * 21)] + ". It has a radius of " + circle.radius + ".");
     circle.logInfo(false);
     console.log("Circle area rounded to the nearest whole number");
     circle.logInfo(true);
