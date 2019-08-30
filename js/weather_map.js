33f34aed583916eb45f63ade578e80e1a242dfc1
BranceA <brancealexander@gmail.com>
2019-08-30 11:28:55 -0500

    added a map and search bar

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 
+    mapboxgl.accessToken = mapboxToken;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 3,
+        center: [-98.5795, 39.8283]
+    });
+
+    map.addControl(new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    }));
+    map.addControl(new mapboxgl.NavigationControl());
+
     $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkskyKey + "/29.4241,-98.4936").done(function (data) {
         console.log(data);
         $("#day-1").html("<p>" + data.daily.data[0].apparentTemperatureLow + "/" + data.daily.data[0].apparentTemperatureHigh + "</p><p>" + data.daily.data[0].icon + "</p><p>" + data.daily.data[0].summary + "</p><p>" + data.daily.data[0].humidity + "</p><p>" + data.daily.data[0].windSpeed + "</p><p>" + data.daily.data[0].pressure + "</p>");
