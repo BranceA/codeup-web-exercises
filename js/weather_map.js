71ab03b42205ffc2c89f95b4a766f33b8bbbc120
BranceA <brancealexander@gmail.com>
2019-08-30 16:16:59 -0500

    wrote a thing that finds the icons

--- js/weather_map.js
+++ js/weather_map.js
@@ -54,8 +54,24 @@ $(document).ready(function () {
         .addTo(map);
 
     $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkskyKey + "/29.4241,-98.4936").done(function (data) {
-        console.log(data);
-        $("#day-1").html("<p>" + data.daily.data[0].apparentTemperatureLow + "/" + data.daily.data[0].apparentTemperatureHigh + "</p><p><img src='" + weatherIcons[2].icon + "'></p><p>" + data.daily.data[0].summary + "</p><p>" + data.daily.data[0].humidity + "</p><p>" + data.daily.data[0].windSpeed + "</p><p>" + data.daily.data[0].pressure + "</p>");
+
+            var nameOfIconOne = data.daily.data[0].icon;
+            var nameOfIconTwo = data.daily.data[1].icon;
+            var nameOfIconThree = data.daily.data[2].icon;
+
+        function findTheIcon (input) {
+            console.log(nameOfIconOne);
+            for (var i = 0; i <= 9; i++) {
+                if (weatherIcons[i].weather === input) {
+                    console.log(weatherIcons[i].icon);
+                    return weatherIcons[i].icon;
+                } else {
+                    console.log("panic");
+                }
+            }
+        };
+
+        $("#day-1").html("<p>" + data.daily.data[0].apparentTemperatureLow + "/" + data.daily.data[0].apparentTemperatureHigh + '</p><p><img src="' + findTheIcon(nameOfIconOne) + '"></p><p>' + data.daily.data[0].summary + "</p><p>" + data.daily.data[0].humidity + "</p><p>" + data.daily.data[0].windSpeed + "</p><p>" + data.daily.data[0].pressure + "</p>");
         $("#day-2").html("<p>" + data.daily.data[1].apparentTemperatureLow + "/" + data.daily.data[1].apparentTemperatureHigh + "</p><p>" + data.daily.data[1].icon + "</p><p>" + data.daily.data[1].summary + "</p><p>" + data.daily.data[1].humidity + "</p><p>" + data.daily.data[1].windSpeed + "</p><p>" + data.daily.data[1].pressure + "</p>");
         $("#day-3").html("<p>" + data.daily.data[2].apparentTemperatureLow + "/" + data.daily.data[2].apparentTemperatureHigh + "</p><p>" + data.daily.data[2].icon + "</p><p>" + data.daily.data[2].summary + "</p><p>" + data.daily.data[2].humidity + "</p><p>" + data.daily.data[2].windSpeed + "</p><p>" + data.daily.data[2].pressure + "</p>");
     });
